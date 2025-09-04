a088b7751ddd5f4c2c4e1efcbc1d7f9b32ab55e8
praveen-h-ami <praveenh@ami.com>
2025-09-04 10:54:17 +0530

    Update CISettings.py
    Keeping Old Submodules

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -193,22 +193,22 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/microsoft/mu_basecore.git",
-                "Branch": "dev/202502"
+                "Branch": "v2025020000.0.0"
             },
             {
                 "Path": "Silicon/Arm/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_silicon_arm_tiano.git",
-                "Branch": "dev/202502"
+                "Branch": "v2025020000.0.0"
             },
             {
                 "Path": "Silicon/Intel/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_silicon_intel_tiano.git",
-                "Branch": "dev/202502"
+                "Branch": "v2025020000.0.0"
             },
             {
                 "Path": "Common/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_tiano_plus.git",
-                "Branch": "dev/202502"
+                "Branch": "v2025020000.0.0"
             }
         ]
 
